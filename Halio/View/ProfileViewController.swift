bf55c198a329feac5f63d9ca1f9ace03438d52d3
Kirill Vikhlyaev <kirillvikhlyaev88@yandex.ru>
2022-09-20 19:16:46 +0500

    add profile page & init gitignore

--- Halio/View/ProfileViewController.swift
+++ Halio/View/ProfileViewController.swift
@@ -8,18 +8,62 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    let pageLabel: UILabel = {
-       let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
-        label.text = "Профиль"
-        label.textColor = .white
-        label.textAlignment = .center
-        label.font = UIFont.boldSystemFont(ofSize: 20.0)
-        return label
-    }()
+
+    let stack: UIStackView = {
+            $0.axis = .vertical
+        $0.alignment = .center
+        $0.distribution = .equalSpacing
+            $0.spacing = 10
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            return $0
+        }(UIStackView())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = K.AppColors.primary
-        self.view.addSubview(pageLabel)
-        pageLabel.center = self.view.center
+        
+        buttonsInit()
+        initStackConstraints()
+
     }
+    
+  
+    func initStackConstraints() {
+        NSLayoutConstraint.activate([
+            stack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: +40),
+            stack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: +40),
+            stack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+        ])
+    }
+    
+    func buttonsInit() {
+        let avatar = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
+        avatar.image = UIImage(named: "placeholder")
+        avatar.layer.masksToBounds = false
+        avatar.clipsToBounds = true
+        avatar.layer.cornerRadius = avatar.frame.height/2
+        avatar.translatesAutoresizingMaskIntoConstraints = false
+        avatar.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        avatar.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        let username = UILabel(frame: CGRect(x: 0, y: 0, width: 150, height: 50))
+        username.text = "Walter White"
+        username.font = .systemFont(ofSize: 21, weight: .bold)
+        username.textColor = K.AppColors.white
+        username.textAlignment = .center
+        
+        
+        let email = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        email.text = "breakingbad@icloud.com"
+        email.font = .systemFont(ofSize: 16, weight: .bold)
+        email.textColor = UIColor.gray
+        email.textAlignment = .center
+        
+        self.view.addSubview(stack)
+        
+        stack.addArrangedSubview(avatar)
+        stack.addArrangedSubview(username)
+        stack.addArrangedSubview(email)
+    }
+
 }
